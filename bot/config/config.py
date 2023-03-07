39b4830e64efc0f139be5e80889d12828a148492
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-03-07 10:15:56 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -5,15 +5,15 @@ load_dotenv("./.env")
 
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1739231223:AAGBcJ3vgHOW_0vvVnoYB61zNgz9hx9vWn0")
     BOT_NAME = os.environ.get("BOT_NAME", "Bot")
 
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "27589257"))
+    API_HASH = os.environ.get("API_HASH", "0af78b04b48361bc117fa4e06d6d2292")
 
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://kk:kk@cluster0.qgpzaa7.mongodb.net/?retryWrites=true&w=majority")
     SESSION_NAME = os.environ.get("DATABASE_NAME", "TelegramBot")
 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
     SUDO_USERS = [int(user) for user in (os.environ.get("SUDO_USERS")).split()]
-    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/")
+    SUPPORT_CHAT_URL = os.environ.get("SUPPORT_CHAT_URL", "https://t.me/drewnotfound")
